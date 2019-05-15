25a3aa8a56d02f049f39d3e78509214fbd38ef1d
Byeongju, Shin <kr.revolt@gmail.com>
2019-05-15 16:33:26 +0900

    jquery추가 및 index.php 헤더 작업

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
 <!DOCTYPE html>
 <html lang="ko">
 <head>
-	<meta charset="UTF-8">
-	<title>정직한 사이트 제목</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="호준과병주">
+    <meta name="description" content="굉장히 어썸한 음악소개 페이지!">
+    <meta name="keywords" content="음악, 힐링, OST, 힙합, 영화음악, 드라마음악, 음악소개">
+
+    <title>정직한 사이트 제목</title>
+
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- HTLM5shiv ie6~8 -->
+    <!--[if lt IE 9]> 
+        <script src="assets/js/html5shiv.min.js"></script>
+        <script type="text/javascript">
+            alert("현재 브라우저는 지원하지 않습니다. 크롬 브라우저를 추천합니다.!");
+        </script>
+    <![endif]-->
 </head>
 <body>
-	
+    <!-- TODO: 헤더 레이아웃 잡기 -->
+    <header id="header">
+        <div class="container">
+        </div>
+    </header>
+
+    <!-- Javascripts -->
+    <script src="assets/js/jquery.min_1.12.4.js"></script>
 </body>
 </html>
